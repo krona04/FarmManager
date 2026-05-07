9bafd04b0824809fd3abe10a5434374b8effaabf
dumitru04 <dimalyt2004@gmail.com>
2026-05-07 23:14:07 +0200

    Alpha v0.4.2

---
+++ Assets/Scripts/MenuMusic.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class MenuMusic : MonoBehaviour
+{
+    [Header("Music Settings")]
+    [Range(0f, 1f)]
+    public float volume = 0.5f;
+    public float fadeInDuration = 2f;
+
+    private AudioSource _audioSource;
+
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        _audioSource.loop = true;
+        _audioSource.volume = 0f;
+        _audioSource.Play();
+
+        StartCoroutine(FadeIn());
+    }
+
+    System.Collections.IEnumerator FadeIn()
+    {
+        float timer = 0f;
+
+        while (timer < fadeInDuration)
+        {
+            timer += Time.deltaTime;
+            _audioSource.volume = Mathf.Lerp(0f, volume, timer / fadeInDuration);
+            yield return null;
+        }
+
+        _audioSource.volume = volume;
+    }
+}
